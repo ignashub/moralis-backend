cb883aa67019b4579d513c7d240edfc597bdc002
Apšega,Ignas I <i.apsega@student.fontys.nl>
2022-04-20 23:14:02 +0200

    js code converted for react

--- react-prototype/src/index.js
+++ react-prototype/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { MoralisProvider } from "react-moralis";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MoralisProvider serverUrl="https://57d0tnsw7vwx.usemoralis.com:2053/server" appId="SOBedjSTVTz5eiaJFRj1DsHUpE9bfbpyUCpZO2ZW">
+    <MoralisProvider
+      serverUrl="https://tnetks9a81s5.usemoralis.com:2053/server"
+      appId="jVerbvE0WJtBWDeK8TVmIKZM58Tt8FT7rtKfuJFF"
+    >
       <App />
     </MoralisProvider>
   </React.StrictMode>
